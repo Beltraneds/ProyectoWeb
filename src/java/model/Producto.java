8cfd7f8d6b77a116585ae9eb417ee623cdf44043
Beltraneds <80973452+Beltraneds@users.noreply.github.com>
2024-11-20 23:57:34 -0300

    Add files via upload

---
+++ src/java/model/Producto.java
@@ -0,0 +1,72 @@
+
+package model;
+
+
+public class Producto {
+    private int id;
+    private String imagen;
+    private String nombre;
+    private String marca;
+    private String descripcion;
+    private String precio;
+
+    public Producto(int id, String imagen, String nombre, String marca, String descripcion, String precio) {
+        this.id = id;
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+    
+    
+        
+}
